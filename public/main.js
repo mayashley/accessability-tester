a90babf84d27ee4826096ad59fb6f7b9a9f6bcea
mayashley <54293844+mayashley@users.noreply.github.com>
2021-11-12 14:11:38 -0500

    bug fix

--- public/main.js
+++ public/main.js
@@ -2,12 +2,26 @@
 const accessibilityTester = async (e) => {
   e.preventDefault();
   const url = document.querySelector("#url").value;
-  console.log(url);
+
+  if (url === "") {
+    alert("Please enter a valid URL");
+  } else {
+    setLoading();
+  }
 };
 //will add issues to the dom
 
 //will set the loading state for our spinner
 
+const setLoading = (isLoading = true) => {
+  const loader = document.querySelector("#loader");
+  if (isLoading) {
+    loader.getElementsByClassName.display = "block";
+  } else {
+    loader.getElementsByClassName.display = "none";
+  }
+};
+
 //esc. our html
 
-document.querySelector("#form").addEventListener("submit", accessibilityTester);
+document.querySelector("#form").addEventListener("button", accessibilityTester);
