1c72cf04a24e9f5aaa5d258ca7ee0b2a36378ae7
mayashley <54293844+mayashley@users.noreply.github.com>
2021-11-12 13:34:01 -0500

    event listener for url entry

--- public/main.js
+++ public/main.js
@@ -1,9 +1,13 @@
 //will get issues
-
+const accessibilityTester = async (e) => {
+  e.preventDefault();
+  const url = document.querySelector("#url").value;
+  console.log(url);
+};
 //will add issues to the dom
 
 //will set the loading state for our spinner
 
 //esc. our html
 
-
+document.querySelector("#form").addEventListener("submit", accessibilityTester);
