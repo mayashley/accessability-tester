2e6791abe51499db0d1a038191db221e69488295
mayashley <54293844+mayashley@users.noreply.github.com>
2021-11-12 15:24:46 -0500

    finished styling and bug fixes

--- public/main.js
+++ public/main.js
@@ -1,73 +1,74 @@
 // Fetch accessibility issues
 const testAccessibility = async (e) => {
-    e.preventDefault()
-  
-    const url = document.querySelector('#url').value
-  
-    if (url === '') {
-      alert('Please add a url')
+  e.preventDefault();
+
+  const url = document.querySelector("#url").value;
+
+  if (url === "") {
+    alert("Please add a vaild url");
+  } else {
+    setLoading();
+
+    const response = await fetch(`/api/test?url=${url}`);
+
+    if (response.status !== 200) {
+      setLoading(false);
+      alert("Something went wrong");
     } else {
-      setLoading()
-  
-      const response = await fetch(`/api/test?url=${url}`)
-  
-      if (response.status !== 200) {
-        setLoading(false)
-        alert('Something went wrong')
-      } else {
-        const { issues } = await response.json()
-        addIssuesToDOM(issues)
-        setLoading(false)
-      }
+      const { issues } = await response.json();
+      addIssuesToDOM(issues);
+      setLoading(false);
     }
   }
-  // issues to DOM
-  const addIssuesToDOM = (issues) => {
-    const issuesOutput = document.querySelector('#issues')
-  
-    issuesOutput.innerHTML = ''
-  
-    if (issues.length === 0) {
-      issuesOutput.innerHTML = '<h4>No Issues Found</h4>'
-    } else {
-      issues.forEach((issue) => {
-        const output = `
+};
+// issues to DOM
+const addIssuesToDOM = (issues) => {
+  const issuesOutput = document.querySelector("#issues");
+
+  issuesOutput.innerHTML = "";
+
+  if (issues.length === 0) {
+    issuesOutput.innerHTML = "<h4>No Issues Found</h4>";
+  } else {
+    issues.forEach((issue) => {
+      const output = `
           <div class="card mb-5">
             <div class="card-body">
               <h4>${issue.message}</h4>
               <p class="bg-light p-3 my-3">
                 ${escapeHTML(issue.context)}
               </p>
-              <p class="bg-secondary text-light p-2">
+              <p class="bg-danger text-light p-2">
                 CODE: ${issue.code}
               </p>
+              <a class="text-dark" target="_blank" href="https://www.w3.org/WAI/standards-guidelines/wcag/">Learn more about WCAG2AA standards</a>
             </div>
           </div>
-        `
-  
-        issuesOutput.innerHTML += output
-      })
-    }
+        `;
+
+      issuesOutput.innerHTML += output;
+    });
   }
-  
-  // Set loading state
-  const setLoading = (isLoading = true) => {
-    const loader = document.querySelector('.loader')
-    if (isLoading) {
-      loader.style.display = 'block'
-    } else {
-      loader.style.display = 'none'
-    }
+};
+
+// Set loading state
+const setLoading = (isLoading = true) => {
+  const loader = document.querySelector(".loader");
+  if (isLoading) {
+    loader.style.display = "block";
+  } else {
+    loader.style.display = "none";
   }
-  
-  // Escape HTML
-  function escapeHTML(html) {
-    return html
-      .replace(/&/g, '&amp;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#039;')
-  }
-  
-  document.querySelector('#form').addEventListener('submit', testAccessibility)
+};
+
+// Escape HTML
+function escapeHTML(html) {
+  return html
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+document.querySelector("#form").addEventListener("submit", testAccessibility);
