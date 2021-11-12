991133675d060c7ce5e5728e054f2c75942063f5
mayashley <54293844+mayashley@users.noreply.github.com>
2021-11-12 14:39:54 -0500

    port fixes, will not run on 5000 so I need to figure out why. For now its on port 3000

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-const pa11y = require("pa11y");
 const express = require("express");
-const { request, response } = require("express");
-//running on port 5001 because 5000 will not run, not sure if its an m1 issues or otherwise...all other ports will run except 5000
-const PORT = process.env.PORT || 5001;
+const pa11y = require("pa11y");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-//middleware for the static folder "might use react later for this but for now its vanilla js"
 app.use(express.static("public"));
 
-app.get("/api/testing", async (request, response) => {
-  if (!request.query.url) {
-    //using a statues of 400 for the client error
-    response.status(400).json({ err: "You must enter a URL, it is required" });
+app.get("/api/test", async (req, res) => {
+  if (!req.query.url) {
+    res.status(400).json({ error: "url is required" });
   } else {
-    const results = await pa11y(request.query.url);
-    //using a status of 200 for OK/success
-    response.status(200).json(results);
+    const results = await pa11y(req.query.url);
+    res.status(200).json(results);
   }
 });
 
-app.listen(PORT, () => console.log("Server has started on port ${PORT}"));
-
-//get request for postman http://localhost:5001/api/testing?url=https://alphatechdevs.com/
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
